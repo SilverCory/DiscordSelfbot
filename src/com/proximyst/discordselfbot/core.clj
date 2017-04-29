994cc68ac780212f1ea67df86ababc744155b796
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-29 22:06:05 +0200

    Warn on reflection, set fallback method, register resolve command, add wrapper listener and remove a TODO

--- src/com/proximyst/discordselfbot/core.clj
+++ src/com/proximyst/discordselfbot/core.clj
@@ -1,11 +1,15 @@
 (ns com.proximyst.discordselfbot.core
   (:gen-class)
-  (:import [net.dv8tion.jda.core JDA JDABuilder]))
+  (:import [net.dv8tion.jda.core JDA JDABuilder]
+           [com.proximyst.discordselfbot.java ListenerWrapper]
+           [net.dv8tion.jda.core.events.message MessageReceivedEvent]
+           [net.dv8tion.jda.core.entities TextChannel User Guild Message]))
 
 (def ^JDA jda)
 
 (defn -main
   [& args]
+  (set! *warn-on-reflection* true)
   (if (= (count args) 0)
     (throw (RuntimeException. "You need to specify a token.")))
   (def jda
@@ -13,8 +17,20 @@
       (JDABuilder.)
       (.setToken (first args))
       (.setCorePoolSize 6)
-      ; TODO: Add message handler here. Use java as a wrapper.
       (.buildBlocking)
       )
     )
+  (set! (ListenerWrapper/jda) jda)
+  (.setFallback (ListenerWrapper/instance)
+                (fn [^MessageReceivedEvent event
+                     ^TextChannel channel
+                     ^User author
+                     ^Guild guild
+                     ^Message message
+                     #^"[Ljava.lang.String;" args]
+                  (.sendMessage channel (str "The command specified doesn't exist: " (first args)))
+                  )
+                )
+  (.registerCommand (ListenerWrapper/instance) "resolve" com.proximyst.discordselfbot.commands.resolve/resolveUrl)
+  (.addEventListener jda (ListenerWrapper/instance))
   )
