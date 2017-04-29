90f0d5bd4a4e96f9755a70ceaa8c334f7a859a8c
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-29 22:54:33 +0200

    Lay more out and register eval and set the engine

--- src/com/proximyst/discordselfbot/core.clj
+++ src/com/proximyst/discordselfbot/core.clj
@@ -3,15 +3,19 @@
   (:import [net.dv8tion.jda.core JDA JDABuilder]
            [com.proximyst.discordselfbot.java ListenerWrapper]
            [net.dv8tion.jda.core.events.message MessageReceivedEvent]
-           [net.dv8tion.jda.core.entities TextChannel User Guild Message]))
+           [net.dv8tion.jda.core.entities TextChannel User Guild Message]
+           [javax.script ScriptEngineManager]))
 
 (def ^JDA jda)
 
 (defn -main
   [& args]
+
   (set! *warn-on-reflection* true)
+
   (if (= (count args) 0)
     (throw (RuntimeException. "You need to specify a token.")))
+
   (def jda
     (->
       (JDABuilder.)
@@ -20,6 +24,7 @@
       (.buildBlocking)
       )
     )
+
   (set! (ListenerWrapper/jda) jda)
   (.setFallback (ListenerWrapper/instance)
                 (fn [^MessageReceivedEvent event
@@ -31,6 +36,14 @@
                   (.sendMessage channel (str "The command specified doesn't exist: " (first args)))
                   )
                 )
+
+  (def com.proximyst.discordselfbot.commands.eval/engine (-> (ScriptEngineManager.) (.getEngineByName "nashorn")))
+
   (.registerCommand (ListenerWrapper/instance) "resolve" com.proximyst.discordselfbot.commands.resolve/resolveUrl)
+
+  ; As long as the eval registering points to the same methods, there should be no problem.
+  (.registerCommand (ListenerWrapper/instance) "eval" com.proximyst.discordselfbot.commands.eval/evaluate)
+  (.registerCommand (ListenerWrapper/instance) "evaluate" com.proximyst.discordselfbot.commands.eval/evaluate)
+
   (.addEventListener jda (ListenerWrapper/instance))
   )
