84caf115674f54f5c2dbae19e54ef3cdca358c8a
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-29 21:17:14 +0200

    Add the initial leiningen project file

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject discordselfbot "0.1.0-SNAPSHOT"
+  :description "A discord selfbot made in Clojure using some Java wrappers & workarounds. "
+  :url "https://github.com/Proximyst/DiscordSelfbot"
+  :license {:name "GNU GPLv3"
+            :url  "https://github.com/Proximyst/DiscordSelfbot/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.8.0"] [net.dv8tion/JDA "3.0.0_188"]]
+  :repositories [["jcenter" "http://jcenter.bintray.com"]]
+  :main ^:skip-aot com.proximyst.discordselfbot.core
+  :java-source-paths ["src"]
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:options"]
+  :global-vars {*warn-on-reflection* true}
+  :uberjar-name "UberJar-%s.jar"
+  :source-paths ["src"]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
