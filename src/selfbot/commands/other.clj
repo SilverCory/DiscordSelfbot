fce90b905a6df0fc2adff26266e48402cdb4c1b5
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-30 23:08:56 +0200

    add now

--- src/selfbot/commands/other.clj
+++ src/selfbot/commands/other.clj
@@ -3,7 +3,9 @@
   (:import [net.dv8tion.jda.core.events.message MessageReceivedEvent]
            [net.dv8tion.jda.core.entities User Guild Message MessageChannel]
            [net.dv8tion.jda.core EmbedBuilder]
-           [java.awt Color]))
+           [java.awt Color]
+           [java.util Date]
+           [java.text SimpleDateFormat]))
 
 (defn pi
   [^MessageReceivedEvent event
@@ -24,4 +26,26 @@
         )
       (.queue)
       )
+  )
+
+(defn now
+  [^MessageReceivedEvent event
+   ^MessageChannel channel
+   ^User author
+   ^Guild guild
+   ^Message message
+   args]
+  (let [time (System/currentTimeMillis)]
+    (-> message
+        (.editMessage
+          (->
+            (EmbedBuilder.)
+            (.setColor (Color/GREEN))
+            (.setTitle "Now" nil)
+            (.addField "Millis/UNIX" (str time "ms since epoch") false)
+            (.addField "Date" (-> (SimpleDateFormat. "EEE, d MMMM yyyy ss:mm:HH") (.format (Date. time))) false)
+            (.build)))
+        (.queue)
+        )
+    )
   )
